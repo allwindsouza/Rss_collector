76e669b0a6485eae97fcd0771770810dbf7e4a89
Allwin Dsouza <allwin.dsouza@amagi.com>
2023-02-22 22:53:33 +0530

    redone the fails

--- xml_utils.py
+++ xml_utils.py
@@ -3,22 +3,25 @@ import requests
 import re
 
 def elements_equal(elem1, elem2):
-    if elem1.tag != elem2.tag:
-        return False
-    if elem1.text != elem2.text:
-        return False
-    if len(elem1) != len(elem2):
-        return False
-    if not all(elements_equal(c1, c2) for c1, c2 in zip(elem1, elem2)):
-        return False
-    if "date" in elem1.attrib or "time" in elem1.attrib:
-        return True  # Return True for date or time-related fields
-    for key in elem1.attrib:
-        if re.search(r"(date|time)", key):
-            return True  # Return True for date or time-related fields
-        if elem1.attrib[key] != elem2.attrib.get(key):
+    try:
+        if elem1.tag != elem2.tag:
             return False
-    return True
+        if elem1.text != elem2.text:
+            return False
+        if len(elem1) != len(elem2):
+            return False
+        if not all(elements_equal(c1, c2) for c1, c2 in zip(elem1, elem2)):
+            return False
+        if "date" in elem1.attrib or "time" in elem1.attrib:
+            return True  # Return True for date or time-related fields
+        for key in elem1.attrib:
+            if re.search(r"(date|time)", key):
+                return True  # Return True for date or time-related fields
+            if elem1.attrib[key] != elem2.attrib.get(key):
+                return False
+        return True
+    except:
+        return False
 
 
 def compare_xml_files(data_1, data_2):
@@ -29,7 +32,6 @@ def compare_xml_files(data_1, data_2):
         # Load XML files into ElementTree objects
         tree1 = ET.ElementTree(ET.fromstring(data_1))
         tree2 = ET.ElementTree(ET.fromstring(data_2))
-
 
         # Get root elements of both trees
         root1 = tree1.getroot()
@@ -43,19 +45,21 @@ def compare_xml_files(data_1, data_2):
                         if not re.search(r"(date|time)", key):
                             if elem1.attrib[key] != elem2.attrib.get(key):
                                 return False
-    except Exception as e:
+    except:
         return False
     return True
 
-def download_and_write(url, path):
-    response = requests.get(url)
-    data = response.text
-    with open(path, 'w') as file:
-        file.write(data)
-#
-#
-file_1 = "/home/allwind/Desktop/CAS/Rss_collector/resources/test_file_1.xml"
-file_2 = "/home/allwind/Desktop/CAS/Rss_collector/resources/test_file_2.xml"
+
+
+# def download_and_write(url, path):
+#     response = requests.get(url)
+#     data = response.text
+#     with open(path, 'w') as file:
+#         file.write(data)
+# #
+# #
+# file_1 = "/home/allwind/Desktop/CAS/Rss_collector/resources/test_file_1.xml"
+# file_2 = "/home/allwind/Desktop/CAS/Rss_collector/resources/test_file_2.xml"
 
 # download_and_write('http://3.220.4.21:3200/rss/f5055/1673600573.5898323.xml',
 #                    '/home/allwind/Desktop/CAS/Rss_collector/resources/test_file_1.xml')
